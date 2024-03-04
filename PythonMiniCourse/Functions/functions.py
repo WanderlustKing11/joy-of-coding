8e3e59ad70890bda0840ae6847ece9728cfd6eb8
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-03-04 10:54:53 -0500

    Initial commit

---
+++ PythonMiniCourse/Functions/functions.py
@@ -0,0 +1,22 @@
+# Learning Python with Joy of Coding
+## Doug's study path
+
+#### Learning about Functions ####
+
+
+# def add(first, second):
+#   print("Sum =", first + second)
+#   return first + second
+# add(3,  4)
+
+##################################
+
+# def add(x,y):
+#   print("I will add the numbers %d and %d" % (x, y))
+#   return x + y
+  
+# sum = add(5, 7)
+# print("=", sum)
+  
+##################################
+
---
+++ PythonMiniCourse/Loops/loops.py
@@ -0,0 +1,72 @@
+# Learning Python with Joy of Coding
+## Doug's study path
+
+
+#### Learning For Loops ####
+#for i in range(1, 13,3):
+ # print(i)
+# 1, 4, 7, 10
+  
+# print('For k:')
+# for k in range(5, -20, -5):
+#   print(k)
+# 5, 0, -5, -10, -15
+
+
+#for letter in range(10):
+#  print('hello')
+# prints 'hello' x10
+
+
+#for i in range(2, -10, 4):
+ # print(i)
+# 
+
+## Challenge ##
+# What does this program output?
+
+# for hi in range(5):
+#   print(hi)
+
+# print("-"*10)
+
+# for lo in range(7, -7, -3):
+#   print(lo)
+  
+# print("-"*10)
+
+# phrase = "Monty Python"
+# for letter in phrase:
+#   print(letter, end="-")
+# print()
+
+# print("End")
+
+## My incorrect prediction:
+# 0
+# 1
+# 2
+# 3
+# 4
+# ----------
+# 7
+# 4
+# 1
+# -2
+# -5
+# ----------
+#M-
+#o-
+#n-
+#t-
+# -
+#P-
+#y-
+#t-
+#h-
+#o-
+#n-
+# END
+## print's default end goes to a new line, but we changed that default
+## `end`
+
